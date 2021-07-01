92992b50f31409843fa8ad3dc05a309ca0ad917e
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-01 17:35:30 +0300

    контроллеры

--- app/script.js
+++ app/script.js
@@ -1,6 +1,7 @@
 let myApp=angular.module('myApp', []);
+
+
 myApp.controller('phoneController', function($scope) {
- 
   $scope.phone = {
         name: 'Nokia Lumia 630',
         year: 2014,
@@ -10,4 +11,10 @@ myApp.controller('phoneController', function($scope) {
             country: 'Финляндия'
         }
     }
+});
+
+
+myApp.controller('myController', function() {
+    this.message ='myController';
+    this.text ='Контроллер без $scope';
 });
