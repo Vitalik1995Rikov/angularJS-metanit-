50189409eb8cf6c4c95530ac47a0fe078d608fa4
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-02 10:10:11 +0300

    директивы управления и стилизации (ng-if, ng-show, ...)

--- app/js/controllers/script.js
+++ app/js/controllers/script.js
@@ -1,38 +1,7 @@
 let myApp = angular.module('myApp', []);
 
 myApp.controller('phoneController', function($scope) {
-    $scope.phones = [{
-        name: 'Nokia Lumia 630',
-        year: 2014,
-        price: 200,
-        company: {
-            name: 'Nokia',
-            country: 'Финляндия'
-            }
-        },{
-            name: 'Samsung Galaxy S 4',
-            year: 2014,
-            price: 400,
-            company: {
-                name: 'Samsung',
-                country: 'Республика Корея'
-            }
-        },{
-            name: 'Mi 5',
-            year: 2015,
-            price: 300,
-            company: {
-                name: 'Xiaomi',
-                country: 'Китай'
-            }
-        },
-        {
-            name: 'Samsung Galaxy S 8',
-            year: 2019,
-            price: 600,
-            company: {
-                name: 'Samsung',
-                country: 'Республика Корея'
-            }
-        }]
-    });
+   $scope.somestyle = {background: '#eee', color: '#f103f3'};
+   $scope.someclass = 'myclass';
+   $scope.data = {visible: false};
+});
