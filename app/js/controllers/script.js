a99b57ab2cb22a6c342382c00c7e153513bf5228
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-02 15:40:32 +0300

    создание собственных фильтров

--- app/js/controllers/script.js
+++ app/js/controllers/script.js
@@ -1,6 +1,46 @@
 let myApp = angular.module('myApp', []);
+let questApp = angular.module('questApp', []);
 
-myApp.controller('QuestionController', function($scope) {
+// myApp.controller('QuestionController', function($scope) {
+//     $scope.sortparam = 'rate';
+//     $scope.question={
+//         text: 'Какой js-фреймворк лучше использовать?',
+//         author: 'Иван Иванов',
+//         date: '20/10/2013',
+//         answers: 
+//         [{
+//             text: 'AngularJS!',
+//             author: 'Вова Сидоров',
+//             date: '20/10/2013',
+//             rate:2
+//         },{
+//             text: 'AngularJS лучше всех',
+//             author: 'Олег Кузнецов',
+//             date: '20/10/2013',
+//             rate:0
+//         },{
+//             text: 'Я бы использовал knockout',
+//             author: 'Неизвестный',
+//             date: '21/10/2013',
+//             rate:0
+//         }]
+//     };
+
+//     $scope.voteUp = function (answer){
+//         answer.rate++;
+//     };
+//     $scope.voteDown = function (answer){
+//         answer.rate--;
+//     };
+//     $scope.questColorClass= "questcolor";
+//     $scope.changeClass = function (e) {
+         
+//         $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
+//     }
+// });
+
+
+questApp.controller('QuestionController', function($scope) {
     $scope.sortparam = 'rate';
     $scope.question={
         text: 'Какой js-фреймворк лучше использовать?',
@@ -18,7 +58,7 @@ myApp.controller('QuestionController', function($scope) {
             date: '20/10/2013',
             rate:0
         },{
-            text: 'Я бы использовал knockout',
+            text: 'фигасе knockout',
             author: 'Неизвестный',
             date: '21/10/2013',
             rate:0
@@ -31,9 +71,15 @@ myApp.controller('QuestionController', function($scope) {
     $scope.voteDown = function (answer){
         answer.rate--;
     };
-    $scope.questColorClass= "questcolor";
-    $scope.changeClass = function (e) {
-         
-        $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
+});
+
+questApp.filter('formatText', function() {
+    return function(text) {
+        if(text.indexOf('фигасе') !== -1) {
+            return '***';
+        } else {
+            return text;
+        }
     }
 });
+
