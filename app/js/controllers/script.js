a6a1c7d9a711b29a8fea600a307e8c239059b6d7
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-02 11:10:07 +0300

    обработка событий

--- app/js/controllers/script.js
+++ app/js/controllers/script.js
@@ -1,7 +1,38 @@
 let myApp = angular.module('myApp', []);
 
-myApp.controller('phoneController', function($scope) {
-   $scope.somestyle = {background: '#eee', color: '#f103f3'};
-   $scope.someclass = 'myclass';
-   $scope.data = {visible: false};
+myApp.controller('QuestionController', function($scope) {
+    $scope.question={
+        text: 'Какой js-фреймворк лучше использовать?',
+        author: 'Иван Иванов',
+        date: '20/10/2013',
+        answers: 
+        [{
+            text: 'AngularJS!',
+            author: 'Вова Сидоров',
+            date: '20/10/2013',
+            rate:2
+        },{
+            text: 'AngularJS лучше всех',
+            author: 'Олег Кузнецов',
+            date: '20/10/2013',
+            rate:0
+        },{
+            text: 'Я бы использовал knockout',
+            author: 'Неизвестный',
+            date: '21/10/2013',
+            rate:0
+        }]
+    };
+
+    $scope.voteUp = function (answer){
+        answer.rate++;
+    };
+    $scope.voteDown = function (answer){
+        answer.rate--;
+    };
+    $scope.questColorClass= "questcolor";
+    $scope.changeClass = function (e) {
+         
+        $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
+    }
 });
