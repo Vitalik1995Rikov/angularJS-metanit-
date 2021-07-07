e18c59887f453dcd7af52ca98a95e686decb2c3e
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-07 09:55:01 +0300

    сервисы

--- app/js/controllers/answerList.js
+++ app/js/controllers/answerList.js
@@ -1,14 +1,10 @@
-questApp.directive('answerList', function () {
-    return function (scope, element, attrs) {
-        let data = scope[attrs['answerList']];
-        if (angular.isArray(data.answers)) {
-            let ulElem = angular.element('<ul>');
-            element.append(ulElem);
-            for(let i = 0; i < data.answers.length; i++) {
-                let liElem = angular.element('<li>');
-                liElem.append(angular.element('<p>').text(data.answers[i].text));
-                ulElem.append(liElem);
-            }
+questApp.controller('QuestionController', 
+    function QuestionController ($scope, dataService) {
+        $scope.question = dataService.question;
+        $scope.voteUp = function (answer) {
+            answer.rate++;
         }
-    }
+        $scope.voteDown = function (answer) {
+            answer.rate--;
+        }
 });
